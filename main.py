975a89fb8a1d33674fed3c4e630094035d22d7b5
huskyth <1622313177@qq.com>
2023-09-14 19:13:25 +0800

    feat: param add

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
     'numIters': 1000,
-    'numEps': 100,  # Number of complete self-play games to simulate during a new iteration.
+    'numEps': 50,  # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 150,  #
     'updateThreshold': 0.6,
     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,  # Number of game examples to train the neural networks.
-    'numMCTSSims': 4000,  # Number of games moves for MCTS to simulate.
-    'arenaCompare': 40,  # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 1.0,
+    'numMCTSSims': 40,  # Number of games moves for MCTS to simulate.
+    'arenaCompare': 30,  # Number of games to play during arena play to determine if new net will be accepted.
+    'cpuct': 1.8,
 
     'checkpoint': './temp/',
     'load_model': True,
